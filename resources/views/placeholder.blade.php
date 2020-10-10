7c06acfbca574a46d23a594e7fb28d9489c84d4f
Adriana Campo <alucaro29a@hotmail.com>
2020-10-10 15:10:37 -0500

    http to https

--- resources/views/placeholder.blade.php
+++ resources/views/placeholder.blade.php
@@ -424,7 +424,7 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.14.0/jquery.validate.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.5/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/plug-ins/f2c75b7247b/integration/bootstrap/3/dataTables.bootstrap.js"></script>
     <script src="https://cdn.datatables.net/responsive/1.0.4/js/dataTables.responsive.js"></script>
